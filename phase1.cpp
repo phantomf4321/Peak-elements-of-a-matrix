ace366fea7b2e4b47b69756f917841afda2501b2
Sajjad Ranjbar <83742735+phantomf4321@users.noreply.github.com>
2021-11-24 19:36:36 +0330

    Create phase1.cpp

---
+++ phase1.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+
+using namespace std;
+
+class matrix
+{
+private:
+	long long int** data;
+	int column, row;
+
+	bool** confirm;
+
+public:
+	matrix(int c, int r) { column = c+2; row = r+2; }
+	void make();
+	void set();
+
+	bool popup(int counter1, int counter2) { if (data[counter1][counter2] > data[counter1 - 1][counter2]) { return true; } }
+	bool popright(int counter1, int counter2) { if (data[counter1][counter2] > data[counter1][counter2 + 1]) { return true; }}
+	bool popdown(int counter1, int counter2) { if (data[counter1][counter2] > data[counter1 + 1][counter2]) { return true; }}
+	bool popleft(int counter1, int counter2) { if (data[counter1][counter2] > data[counter1][counter2 - 1]) { return true; }}
+
+	void check();
+	void print();
+
+};
+
+void matrix::make()
+{
+	confirm = new bool* [row];
+	for (int i = 0; i < row; ++i)
+		confirm[i] = new bool[column];
+
+	data = new long long int* [row];
+	for (int i = 0; i < row; ++i)
+		data[i] = new long long int[column];
+
+	for (int counter1 = 0; counter1 < row; counter1++)
+		for (int counter2 = 0; counter2 < column; counter2++)
+			if (counter1 == 0 || counter2 == 0 || counter1 == row - 1 || counter2 == column - 1)
+				data[counter1][counter2] = INT_MIN;
+}
+void matrix::set()
+{
+	make();
+	for (int counter1 = 1; counter1 <= row - 2; counter1++)
+		for (int counter2 = 1; counter2 <= column - 2; counter2++)
+		{
+			cin >> data[counter1][counter2];
+			confirm[counter1][counter2] = false;
+		}
+}
+void matrix::check()
+{
+
+	for (int counter1 = 1; counter1 <= row - 2; counter1++)
+		for (int counter2 = 1; counter2 <= column - 2; counter2++)
+			if (popup(counter1, counter2) == true && popright(counter1, counter2) == true && popdown(counter1, counter2) == true && popleft(counter1, counter2) == true)
+				confirm[counter1][counter2] = true;
+}
+void matrix::print()
+{
+	for (int counter1 = 1; counter1 < row - 1; counter1++)
+		for (int counter2 = 1; counter2 < column - 1; counter2++)
+			if (confirm[counter1][counter2])
+				cout << data[counter1][counter2] << endl;
+}
+
+
+int main()
+{
+	int column, row;
+	cin >> column >> row;
+	matrix m1(column, row);
+	m1.set();
+
+	m1.check();
+	m1.print();
+
+
+	system("pause");
+	return 0;
+}
